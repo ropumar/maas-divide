bba058ba5178bef43f773cf53c9aff72ed431932
Rodrigo Pumar <rodrigo.pumar@b2wdigital.com>
2019-06-18 16:53:01 -0300

    initial

--- maas/app.py
+++ maas/app.py
@@ -6,4 +6,18 @@ app = App("", "", "", 1)
 
 @app.route(["/"], type=RouteTypes.HTTP, methods=["POST"])
 async def operation(request: web.Request):
-    return web.json_response({})
+    data = await request.json()
+    try:
+        result = division (data['left'],data['right'])
+        if type(result)!=int:
+            result = int(result)
+        myjson = {'result': result}
+    except:
+        myjson={}
+    return web.json_response(myjson)
+
+def division(a, b):
+    if b==0:
+        raise Exception("Division by zero")
+    return a/b
+
